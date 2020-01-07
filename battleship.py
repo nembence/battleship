2f8b68331705cb3005000d0d037ee1ae05e2bba7
nembence <nemeth.bence@icloud.com>
2020-01-07 14:03:08 +0100

    add print_board function

--- battleship.py
+++ battleship.py
@@ -11,12 +11,27 @@ def get_board():
 def valid_input():
     while True:
         coordinate,direct=get_input()
-        if (coordinate[0].upper() in string.ascii_uppercase[0:5] and int(coordinate[1]) in range(1,6)):
-            break
+        if len(coordinate) == 2:
+            if (coordinate[0].upper() in string.ascii_uppercase[0:5] and int(coordinate[1]) in range(1,6)):
+                break
         else:
             print('Invalid input!')
             continue 
-    return coordinate,direct   
+    return coordinate,direct
+
+def print_board(board):
+    print('  ',end="")
+    for number in range(1,6):
+        print(str(number)+' ',end='')
+    print('\r')    
+    for row in range(len(board)):
+        print(string.ascii_uppercase[row]+' ',end="") 
+        for column in range(len(board[row])):  
+            if board[row][column]==1:
+                print('X ',end="")
+            else:
+                print('0 ',end="") 
+        print('\r')
 
 def get_input():
     coordinate=input('Your coordinate: ')
@@ -25,7 +40,9 @@ def get_input():
 
 def main():
     board=get_board()
+    board[0][0]=1
     coordinate,direct=valid_input()
+    print_board(board)
 
 if __name__ == "__main__":
     main()                
