4b7fe542dfba40bd37ef22fe54e19a3e5566409a
nembence <nemeth.bence@icloud.com>
2020-01-07 13:18:49 +0100

    add get_board function

---
+++ battleship.py
@@ -0,0 +1,20 @@
+def get_board():
+    board = []
+    for row in range(5):
+        board.append([])
+        for column in range(5):
+            board[i].append(0)
+    return board        
+
+def print_board():
+    for row in range(len(board)):
+        for column in range(row):
+            
+            
+
+
+def main():
+    board=get_board()
+
+if __name__ == "__main__":
+    main()                
