aa41fc2941f7eb0373ac38c4d74d11e0f8ca1cf3
nembence <nemeth.bence@icloud.com>
2020-01-07 14:49:04 +0100

    add place_ship function

--- battleship.py
+++ battleship.py
@@ -1,4 +1,6 @@
 import string
+import os
+import time
 
 def get_board():
     board = []
@@ -10,13 +12,18 @@ def get_board():
 
 def valid_input():
     while True:
-        coordinate,direct=get_input()
+        coordinate,direct = get_input()
         if len(coordinate) == 2:
             if (coordinate[0].upper() in string.ascii_uppercase[0:5] and int(coordinate[1]) in range(1,6)):
                 break
         else:
             print('Invalid input!')
             continue 
+    return coordinate,direct
+
+def get_input():
+    coordinate = input('Your coordinate: ')
+    direct = input('Vertical or horizontal (v/h): ')  
     return coordinate,direct
 
 def print_board(board):
@@ -27,22 +34,48 @@ def print_board(board):
     for row in range(len(board)):
         print(string.ascii_uppercase[row]+' ',end="") 
         for column in range(len(board[row])):  
-            if board[row][column]==1:
+            if board[row][column] == 1:
                 print('X ',end="")
             else:
                 print('0 ',end="") 
         print('\r')
 
-def get_input():
-    coordinate=input('Your coordinate: ')
-    direct=input('Vertical or horizontal (v/h): ')  
-    return coordinate,direct
+
+def place_ship(ships,board,coordinate,direct):
+    if ships[0] == 1:
+        letter = coordinate[0].upper()
+        row=string.ascii_uppercase.index(letter)
+        column = int(coordinate[1])-1
+        board[row][column] = 1
+    else:
+        if direct == 'v':
+            letter = coordinate[0].upper()
+            row=string.ascii_uppercase.index(letter)
+            column = int(coordinate[1])-1
+            board[row][column] = 1
+            board[row+1][column] = 1
+        else:
+            letter = coordinate[0].upper()
+            row=string.ascii_uppercase.index(letter)
+            column = int(coordinate[1])-1
+            board[row][column] = 1
+            board[row][column+1] = 1    
+
+def player1_place():
+    os.system('clear')
+    player1 = get_board()
+    ships = [1,1,1,2,2]
+    while len(ships) != 0:
+        print_board(player1)
+        coordinate,direct = valid_input()
+        place_ship(ships,player1,coordinate,direct)
+        del ships[0]
+        time.sleep(1)
+        os.system('clear')         
 
 def main():
-    board=get_board()
-    board[0][0]=1
-    coordinate,direct=valid_input()
-    print_board(board)
+    os.system('clear')
+    player1_place()
 
 if __name__ == "__main__":
     main()                
